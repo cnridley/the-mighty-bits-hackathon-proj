434a8f129d0054c87aac2d1ec0ac2783e307a163
cnridley <cnridley1@gmail.com>
2021-03-07 23:57:27 +0000

    updated indentation errors

--- paddys_trivia/settings.py
+++ paddys_trivia/settings.py
@@ -83,14 +83,14 @@ WSGI_APPLICATION = 'paddys_trivia.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATABASES ={
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    } 
+    }
 else:
     DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
    }
 }
 
